6b9079583258b0f1b758fd3663ceb732b76b24c1
mnjp143 <101368476+mnjp143@users.noreply.github.com>
2022-12-05 18:19:26 +0100

    Add files via upload

--- connection.cpp
+++ connection.cpp
@@ -1,15 +1,23 @@
-    #include "connection.h"
+#include "connection.h"
 
-Connection::Connection(){}
+Connection::Connection()
+{
+
+}
+
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("test-bd");
-db.setUserName("oussema");//inserer nom de l'utilisateur
-db.setPassword("password");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("mybase");
+db.setUserName("maroua");//inserer nom de l'utilisateur
+db.setPassword("maroua");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
+
+
+
+
+
     return  test;
 }
-void Connection::closeConnection(){db.close();}
